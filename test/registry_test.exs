3840f8ae73c037bfa1b8959dc6176ea56cccee17
Seahouse <valentin.seehausen@seahouse.me>
2016-01-06 18:23:02 +0100

    first commit

---
+++ test/registry_test.exs
@@ -0,0 +1,35 @@
+defmodule KV.RegistryTest do
+	use ExUnit.Case, async: true
+
+	setup context do
+		{:ok, registry} = KV.Registry.start_link(context.test)
+		{:ok, registry: registry}
+	end
+
+	test "spawns buckets", %{registry: registry} do
+		assert KV.Registry.lookup(registry, "shopping") == :error
+
+		KV.Registry.create(registry, "shopping")
+		assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+		KV.Bucket.put(bucket, "milk", 1)
+		assert KV.Bucket.get(bucket, "milk") == 1
+	end
+
+	test "removes buckets on exit", %{registry: registry} do
+		KV.Registry.create(registry, "shopping")
+		{:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+		Agent.stop(bucket)
+		assert KV.Registry.lookup(registry, "shopping") == :error
+	end
+
+	test "removes bucket on crash", %{registry: registry} do
+		KV.Registry.create(registry, "shopping2")
+		{:ok, bucket} = KV.Registry.lookup(registry, "shopping2")
+
+		# Kill the bucket and wait for the notification
+		Process.exit(bucket, :shutdown)
+		assert_receive {:exit, "shopping2", ^bucket}
+		assert KV.Registry.lookup(registry, "shopping2") == :error
+	end
+end
