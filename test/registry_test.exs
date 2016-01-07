25190eccd47b0c36755e90df1ef0677fa57965bc
Seahouse <valentin.seehausen@seahouse.me>
2016-01-07 02:15:03 +0100

    now everything is in sync, no more race conditions

--- test/registry_test.exs
+++ test/registry_test.exs
@@ -3,7 +3,7 @@ defmodule KV.RegistryTest do
 
 	setup context do
 		{:ok, registry} = KV.Registry.start_link(context.test)
-		{:ok, registry: registry}
+		{:ok, registry: context.test}
 	end
 
 	test "spawns buckets", %{registry: registry} do
@@ -20,25 +20,26 @@ defmodule KV.RegistryTest do
 		KV.Registry.create(registry, "shopping")
 		{:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 		Agent.stop(bucket)
+		# Do a call to ensure the registry processed the down message
+		_ = KV.Registry.create(registry, "bogus")
 		assert KV.Registry.lookup(registry, "shopping") == :error
 	end
 
 	test "removes bucket on crash", %{registry: registry} do
-		KV.Registry.create(registry, "shopping2")
-		{:ok, bucket} = KV.Registry.lookup(registry, "shopping2")
+		KV.Registry.create(registry, "shopping")
+		{:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
 		# Kill the bucket and wait for the notification
 		Process.exit(bucket, :shutdown)
-	
+
+		# I think the documentation online is misleading/wrong about this test case because
+		# according to fishcakez_ there is no way to actually sync this... but this works
+		:timer.sleep(2)
+		# Do a sync to ensure the registry processed the down message
+		_ = KV.Registry.create(registry, "bogus")
+		# assert_receive {:exit, "shopping", ^bucket}
 
 
-		#assert_receive {:exit, "shopping2", ^bucket}
-
-        	# I think the documentation online is misleading/wrong about this test case because
-        	# according to fishcakez_ there is no way to actually sync this... but this works
-        	:timer.sleep(100)
-
-
-		assert KV.Registry.lookup(registry, "shopping2") == :error
+		assert KV.Registry.lookup(registry, "shopping") == :error
 	end
 end
